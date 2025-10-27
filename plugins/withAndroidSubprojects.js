a1b2cde23996466901461cee7f214f042c15bf0c
ndrkltsk <aklitsuk@gmail.com>
2025-10-27 16:55:52 +0100

    refactor: improve variable naming and streamline configuration handling in withAndroidSubprojects.js

--- plugins/withAndroidSubprojects.js
+++ plugins/withAndroidSubprojects.js
@@ -1,8 +1,8 @@
 const { withProjectBuildGradle } = require('@expo/config-plugins');
 
 const withAndroidSubprojects = config => {
-  return withProjectBuildGradle(config, config => {
-    if (config.modResults.language === 'groovy') {
+  return withProjectBuildGradle(config, modConfig => {
+    if (modConfig.modResults.language === 'groovy') {
       // Add subprojects block after allprojects
       const subprojectsBlock = `
 subprojects {
@@ -17,21 +17,21 @@ subprojects {
 `;
 
       // Insert the subprojects block after the allprojects block
-      if (!config.modResults.contents.includes('subprojects {')) {
-        const allProjectsEndIndex = config.modResults.contents.indexOf(
+      if (!modConfig.modResults.contents.includes('subprojects {')) {
+        const allProjectsEndIndex = modConfig.modResults.contents.indexOf(
           '}',
-          config.modResults.contents.indexOf('allprojects {')
+          modConfig.modResults.contents.indexOf('allprojects {')
         );
         if (allProjectsEndIndex !== -1) {
-          config.modResults.contents =
-            config.modResults.contents.slice(0, allProjectsEndIndex + 1) +
+          modConfig.modResults.contents =
+            modConfig.modResults.contents.slice(0, allProjectsEndIndex + 1) +
             '\n' +
             subprojectsBlock +
-            config.modResults.contents.slice(allProjectsEndIndex + 1);
+            modConfig.modResults.contents.slice(allProjectsEndIndex + 1);
         }
       }
     }
-    return config;
+    return modConfig;
   });
 };
 
