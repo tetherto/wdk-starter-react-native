1054cb9b4a86a2f405661217523a0f76d87bbf58
S.Zviahelskyi <zvyahelskiy@gmail.com>
2025-12-05 11:57:12 +0200

    refactor(tests): simplify Jest config and add unit tests for formatAmount

--- jest.config.js
+++ jest.config.js
@@ -5,9 +5,4 @@ module.exports = {
   transform: {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
-  transformIgnorePatterns: [
-    // Ignore all node_modules except packages that use ESM or need to be transpiled
-    // This allows Jest to process modern Expo/React Native packages correctly
-    'node_modules/(?!(react-native|@react-native|expo|@expo|@tetherto/wdk-uikit-react-native|expo-modules-core)/)',
-  ],
 };
